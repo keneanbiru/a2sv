06288dd14a2e083cb0f8b740135473342db80302
keneanbiru <124770315+keneanbiru@users.noreply.github.com>
2024-02-24 07:25:11 -0800

    Add solution for Transpose Matrix

--- leetcode/transpose-matrix.py
+++ leetcode/transpose-matrix.py
@@ -1,11 +1,3 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        transposed=[[0]*len(matrix) for _ in range (len(matrix[0]))]
-        rlen =len(matrix)
-        clen=len(matrix[0])
-        for i in range (rlen):
-            for j in range(clen):
-                transposed[j][i]=matrix[i][j]
-        return transposed
-
-        
+        return list(zip(*matrix))

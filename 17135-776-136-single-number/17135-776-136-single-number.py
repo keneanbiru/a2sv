42c86b1c4b671ab2cca16b689993e3181a37f69d
keneanbiru <124770315+keneanbiru@users.noreply.github.com>
2025-01-31 20:10:33 +0300

    Time: 7 ms, Memory: 19.9 MB - LeetHub

---
+++ 17135-776-136-single-number/17135-776-136-single-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        for key,val in cnt.items():
+            if val == 1:
+                return key

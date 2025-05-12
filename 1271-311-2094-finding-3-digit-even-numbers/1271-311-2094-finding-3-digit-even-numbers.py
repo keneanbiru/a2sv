f5a4d34a109a7f38aefaf83e913a04b86d7e9aa6
keneanbiru <124770315+keneanbiru@users.noreply.github.com>
2025-05-12 11:10:32 -0800

    Time: 3 ms, Memory: 17.8 MB - LeetHub

---
+++ 1271-311-2094-finding-3-digit-even-numbers/1271-311-2094-finding-3-digit-even-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        freq = [0] * 10
+        for digit in digits:
+            freq[digit] += 1
+        res = []
+        for i in range(1, 10):
+            if freq[i] == 0: continue
+            freq[i] -= 1
+            for j in range(10):
+                if freq[j] == 0: continue
+                freq[j] -= 1
+                for k in range(0, 9, 2):
+                    if freq[k] == 0: continue
+                    res.append(i * 100 + j * 10 + k)
+                freq[j] += 1
+            freq[i] += 1
+        return res
